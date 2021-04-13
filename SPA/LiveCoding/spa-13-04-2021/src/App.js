e59140820ee91fb5a4195b09a20969d295b0a72e
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-04-13 09:30:59 +0200

    spa-13-04-2021 p2

--- SPA/LiveCoding/spa-13-04-2021/src/App.js
+++ SPA/LiveCoding/spa-13-04-2021/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
+function Navbar() {
+  return <nav className="navbar">navbar</nav>;
+}
+
+function Main() {
+  return <div className="main">main</div>;
+}
+
+function Footer() {
+  return <footer className="footer">footer</footer>;
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
