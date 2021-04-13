70e1afae3c371432c1effef40e11bb2981170db8
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-04-13 10:22:33 +0200

    spa-13-04-2021 p3

--- SPA/LiveCoding/spa-13-04-2021/src/components/Navbar/Navbar.js
+++ SPA/LiveCoding/spa-13-04-2021/src/components/Navbar/Navbar.js
@@ -4,8 +4,9 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="container">
+        <input type="checkbox" id="input-menu" />
         <div className="subNavbar">
-          <label className="label" htmlFor="">
+          <label className="label" htmlFor="input-menu">
             <i class="fas fa-bars"></i>
           </label>
           <div className="Logo">Logo</div>
