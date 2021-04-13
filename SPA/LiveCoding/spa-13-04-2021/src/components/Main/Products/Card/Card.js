cda4cb8d90bf9c0f6407ee31db1d559978cbd927
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-04-13 11:27:21 +0200

    spa-13-04-2021 p6

--- SPA/LiveCoding/spa-13-04-2021/src/components/Main/Products/Card/Card.js
+++ SPA/LiveCoding/spa-13-04-2021/src/components/Main/Products/Card/Card.js
@@ -1,17 +1,18 @@
+let title = "Card title";
+let content =
+  " Some quick example text to build on the card title and make up the bulk of the card's content.";
+
 function Card() {
   return (
-    <div class="card" style={{ width: "18rem" }}>
+    <div class="card" style={{ width: "100%" }}>
       <img
         src="https://static.zajo.net/content/mediagallery/zajo_dcat/image/product/types/X/9087.png"
         class="card-img-top"
         alt="..."
       />
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{content}</p>
         <a href="#" class="btn btn-primary">
           Go somewhere
         </a>
