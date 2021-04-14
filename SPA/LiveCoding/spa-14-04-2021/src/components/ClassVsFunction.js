2bcf18da9ded72e5f861e894cb7a8de375a021c3
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-04-14 10:50:21 +0200

    spa-14-04-2021 p3

--- SPA/LiveCoding/spa-14-04-2021/src/components/ClassVsFunction.js
+++ SPA/LiveCoding/spa-14-04-2021/src/components/ClassVsFunction.js
@@ -3,10 +3,14 @@ import React, { Component, useState } from "react";
 export function FunctionComponent() {
   const [name, setName] = useState("Alex");
   const [email, setEmail] = useState("alex@gmail.com");
+  const onChangeUser = () => {
+    setName("Pilar");
+    setEmail("pilar@gmail.com");
+  };
   return (
     <div>
       <h1>Function Component</h1>
-      <button onClick={}>Click me!</button>
+      <button onClick={onChangeUser}>Click me!</button>
       <div>user name: {name}</div>
       <div>user email: {email}</div>
     </div>
@@ -19,7 +23,7 @@ export class ClassComponent extends Component {
     email: "alex@gmail.com",
   };
 
-  onChangeName = () => {
+  onChangeUser = () => {
     this.setState({ name: "Max", email: "max@gmail.com" });
   };
 
@@ -27,7 +31,7 @@ export class ClassComponent extends Component {
     return (
       <div>
         <h1>Class Component</h1>
-        <button onClick={this.onChangeName}>Click me!</button>
+        <button onClick={this.onChangeUser}>Click me!</button>
         <div>user name: {this.state.name}</div>
         <div>user email: {this.state.email}</div>
       </div>
