958c515efb96dc788270e13c7c2e1a6b78e50eef
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-04-26 10:04:42 +0200

    26-04-21

---
+++ SPA/LiveCoding/spa-26-04-2021/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import SimpleForm from "./components/SimpleForm";
+
+function App() {
+  return (
+    <div className="Container">
+      <h1>Forms and Events</h1>
+      <SimpleForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ SPA/LiveCoding/spa-26-04-2021/src/components/SimpleForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const inputStyles = { marginBottom: "10px" };
+function SimpleForm() {
+  const [firstInputValue, setFirstInputValue] = useState("");
+  const [secondInputValue, setSecondInputValue] = useState("");
+  const onFirstInput = (e) => {
+    //let inputData = e.target.value;
+    //setFirstInputValue(inputData);
+    //console.log(inputData);
+    let { value } = e.target;
+    setFirstInputValue(value);
+  };
+
+  const onSecondInput = (e) => {
+    setSecondInputValue(e.target.value);
+  };
+  return (
+    <div>
+      <h2>Simple Form</h2>
+      <form>
+        <div style={inputStyles}>
+          <label htmlFor="input-1">First input: </label>
+          <input onChange={onFirstInput} id="input-1" type="text" />
+        </div>
+        <div>
+          <label htmlFor="input-2">Second input: </label>
+          <input onChange={onSecondInput} id="input-2" type="text" />
+        </div>
+      </form>
+      <div>
+        <p>Data from first input: {firstInputValue}</p>
+        <p>Data from second input: {secondInputValue}</p>
+      </div>
+    </div>
+  );
+}
+
+export default SimpleForm;
