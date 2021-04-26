01df378833b620c02cd92335ea1281697870d19e
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-04-26 13:30:36 +0200

    move array data to data.js in component

--- SPA/LiveCoding/spa-26-04-2021/src/components/CardsForm.js
+++ SPA/LiveCoding/spa-26-04-2021/src/components/CardsForm.js
@@ -1,22 +1,11 @@
 import React, { useState } from "react";
 import Card from "./Card";
+import cardsDataArray from "./data";
 
 export default function CardsForm() {
   const [cardTitle, setCardTitle] = useState("");
   const [cardImg, setCardImg] = useState("");
-
-  const [cardsData, setCardsData] = useState([
-    {
-      cardTitle: "First card title",
-      img:
-        "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000",
-    },
-    {
-      cardTitle: "BMW",
-      img:
-        "https://www.bmwgroup.com/content/dam/grpw/websites/bmwgroup_com/brands/einstiegsseite/1280x854_P90351044_highRes_the-new-bmw-8-series.jpg",
-    },
-  ]);
+  const [cardsData, setCardsData] = useState(cardsDataArray);
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
---
+++ SPA/LiveCoding/spa-26-04-2021/src/components/data.js
@@ -0,0 +1,14 @@
+const cardsDataArray = [
+  {
+    cardTitle: "First card title",
+    img:
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-gold-select-201810?wid=892&hei=820&&qlt=80&.v=1603332211000",
+  },
+  {
+    cardTitle: "BMW",
+    img:
+      "https://www.bmwgroup.com/content/dam/grpw/websites/bmwgroup_com/brands/einstiegsseite/1280x854_P90351044_highRes_the-new-bmw-8-series.jpg",
+  },
+];
+
+export default cardsDataArray;
