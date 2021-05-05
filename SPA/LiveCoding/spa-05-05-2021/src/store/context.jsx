8bb7291a4be92d5fa5b0de433d70eddc59ce0702
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2021-05-05 14:50:36 +0200

    template-05-05-21 context solve the bug

--- SPA/LiveCoding/spa-05-05-2021/src/store/context.jsx
+++ SPA/LiveCoding/spa-05-05-2021/src/store/context.jsx
@@ -16,34 +16,71 @@ const Context = createContext({
   isValidEmail: false,
   password: "",
   isValidPassword: false,
-  onSignHandler: (e) => {},
+  onSignHandler: () => {},
   onEmailHandler: (e) => {},
-  onCheckEmailHandler: (e) => {},
+  onCheckEmailHandler: () => {},
   onPasswordHandler: (e) => {},
-  onPasswordCheckHandler: (e) => {},
+  onPasswordCheckHandler: () => {},
 });
 
 const Provider = (props) => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const [isValidEmail, setIsValidEmail] = useState();
-  const [isValidPassword, setIsValidPassword] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
 
+  const validator = (param) => {
+    let isValid = false;
+
+    // find the index of object with the current email:)
+    let index = DBUsers.findIndex((user) => user.email === email);
+    console.log("user ==> ", index);
+    // you can use forEach or map
+    DBUsers.forEach((user, i) => {
+      if (index == i) {
+        if (param === "email" && user.email === email) {
+          isValid = true;
+          setIsValidEmail(true);
+        }
+        if (param === "password" && user.password === password) {
+          isValid = true;
+          setIsValidPassword(true);
+        }
+      }
+    });
+
+    return isValid;
+  };
   const onEmailHandler = (e) => {
     let value = e.target.value.trim().toLowerCase();
     setEmail(value);
   };
-  const onCheckEmailHandler = (e) => {
-    //console.log("onCheckEmailHandler ");
+  const onCheckEmailHandler = () => {
+    const isValid = validator("email");
+    if (isValid) return;
+    alert("The Email is not valid");
+    setIsValidEmail(false);
   };
   const onPasswordHandler = (e) => {
     let value = e.target.value.trim();
     setPassword(value);
   };
-  const onPasswordCheckHandler = (e) => {
-    //console.log("onPasswordCheckHandler ");
+  const onPasswordCheckHandler = () => {
+    const isValid = validator("password");
+    if (isValid) return;
+    // alert("The Password is not valid");
+    setIsValidPassword(false);
   };
-  const onSignHandler = (e) => {};
+  const onSignHandler = () => {
+    if (!isValidEmail) {
+      alert("The Email is not valid");
+    } else if (!isValidPassword) {
+      alert("The Password is not valid");
+    } else {
+      localStorage.setItem("token", true);
+      window.location.reload();
+    }
+  };
   return (
     <Context.Provider
       value={{
